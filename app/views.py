a0eecc8dd00a63dca193a1622c8ea2126dd3afc7
cospamental <cospa18@gmail.com>
2018-01-26 18:02:50 +0300

    changed sports to general

--- app/views.py
+++ app/views.py
@@ -10,7 +10,8 @@ def index():
     View root page function that returns the index page and its data
     '''
     # Get news
-    top_headlines = get_news('sports')
+    top_headlines = get_news('general')
+    
     print(top_headlines)
     title = 'News'
     return render_template('index.html', title = title,trending = top_headlines)
