37019fd67ac7ac2e1854d46f9e9d517d0a073323
cospamental <cospa18@gmail.com>
2018-01-26 17:06:10 +0300

    entered the sports list

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ def index():
     View root page function that returns the index page and its data
     '''
     # Get news
-    top_headlines = get_news()
+    top_headlines = get_news('sports')
     print(top_headlines)
     title = 'News'
     return render_template('index.html', title = title,trending = top_headlines)
