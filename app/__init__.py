65b6026028be54010ca228c24bf04396927fcd5d
cospamental <cospa18@gmail.com>
2018-01-26 12:11:39 +0300

    added the instance relative to connect with the instance folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from .config import DevConfig
 
-app =   Flask(__name__)
+app =   Flask(__name__,instance_relative_config=True)
 
 # setting up configuration
 app.config.from_object(DevConfig)
