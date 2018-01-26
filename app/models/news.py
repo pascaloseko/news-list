f408ec6a5054a336a524c5023d1c1bc916c0bfed
cospamental <cospa18@gmail.com>
2018-01-26 14:24:53 +0300

    added process results on the requests

---
+++ app/models/news.py
@@ -0,0 +1,9 @@
+class News:
+    def __init__(self,author,title,description,url,urlToImage,publishedAt):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
+ 
--- app/requests.py
+++ app/requests.py
@@ -1,4 +1,37 @@
 from app import app
+import urllib.request,json
+from .models import news
+
+News = news.News
 
 # Getting the api key
 api_key = app.config['NEWS_API_KEY']
+
+# Getting the news base url
+base_url = app.config["NEWS_API_BASE_URL"]
+
+def get_news(category):
+    get_news_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_news_url) as url:
+        get_news_data = url.read()
+        get_news_respose = json.loads(get_news_data)
+
+        news_results = None
+
+        if get_news_response['results']:
+            news_results_list = get_news_res['results']
+            news_results = process_results(news_results_list)
+
+    return news_results
+
+def process_results(news_list):
+    '''
+    Function  that processes the movie result and transform them to a list of Objects
+
+    Args:
+        news_list: A list of dictionaries that contain movie details
+
+    Returns :
+        news_results: A list of movie objects
+    '''
