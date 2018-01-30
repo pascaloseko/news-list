eacb74a2d4c1b30198844e8523802d9ff71850f7
cospamental <cospa18@gmail.com>
2018-01-30 21:12:27 +0300

    added categories for display

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,7 @@
 from flask import render_template,request
 from . import main
-from ..requests import get_news
-from ..models import Sources
+from ..requests import get_news_sources,get_sources_articles
+from ..models import Source
 
 # Views
 @main.route('/')
@@ -11,20 +11,33 @@ def index():
     View root page function that returns the index page and its data
     '''
     # Get news
-    top_headlines = get_news('general')
-    sports        = get_news('sports')
-    tech           = get_news('technology')
-    print(top_headlines)
-    title = 'News'
-    return render_template('index.html', title = title,trending = top_headlines,sports=sports,techie=tech)
+    general       = get_news_sources('gb','general')
+    #print(general)
+    sports        = get_news_sources('gb','sports')
+    tech          = get_news_sources('us','technology')
+    #print(tech)
+    business      = get_news_sources('gb','business')
+    #print(business)
+    health        = get_news_sources('us','health')
+    #print(health)
+    entertainment = get_news_sources('gb','entertainment')
+    #print(entertainment)
+    #print(top_headlines)
+    #title = 'News'
+    return render_template('index.html', 
+                            general = general,
+                            sports=sports,
+                            techie=tech,
+                            business=business,
+                            health=health,
+                            entertainment=entertainment)
 
-@main.route('/sources/<string:id>')
-def sources(id):
+@main.route('/news/<id>')
+def news(id):
+    """
+    View articles page that returns the news article from a highlight
+    """
+    news = get_sources_articles(id)
 
-    '''
-    View sources page function that returns the source details page and its data
-    '''
-    sources = Sources.get_sources(sources.id)
-    print(sources)
+    return render_template('sources.html',news=news)
 
-    return render_template('sources.html',sources = sources)
